5d1f225310d7476e6316ecd2cd8953b9cc67dbc6
Kir Tananushka <kir_tananushka@epam.com>
2025-03-17 22:25:02 +0400

    Update security group configuration using a module

--- main.tf
+++ main.tf
@@ -25,11 +25,25 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id  # Use the fetched AMI ID
   instance_type = var.instance_type  # Instance type (e.g., "t2.micro", "t3.medium", etc.)
 
-  vpc_security_group_ids = [aws_security_group.blog.id]  # Attach the security group to control traffic
+  vpc_security_group_ids = [module.blog_sg.security_group_id]  # Attach the security group to control traffic
 
   tags = {
     Name = "HelloWorld"  # Assign a name tag for easy identification
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name = "blog_new"
+  
+  vpc_id = data.aws_vpc.default.id  # Associate this security group with the default VPC
+
+  ingress_rules      = ["http-80-tcp", "https-443-tcp"]  # Allow incoming HTTP and HTTPS connections
+  ingress_cidr_blocks = ["0.0.0.0/0"]  # Open to the entire internet (any IP address)
+
+  egress_rules      = ["all-all"]  # Allow incoming HTTP and HTTPS connections
+  egress_cidr_blocks = ["0.0.0.0/0"]  # Open to the entire internet (any IP address)
 }
 
 # Create a security group for the EC2 instance
