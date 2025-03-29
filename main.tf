ed6979bb215757c7fad1e3851c7a8a75f57f3c96
Kir Tananushka <kir_tananushka@epam.com>
2025-03-29 23:18:31 +0400

    Add custom VPC module and update resources to use it

--- main.tf
+++ main.tf
@@ -20,6 +20,21 @@ data "aws_vpc" "default" {
   default = true  # Gets the default VPC automatically created by AWS
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 # Create an EC2 instance using the latest Bitnami Tomcat AMI
 resource "aws_instance" "blog" {
   ami = data.aws_ami.app_ami.id  # Use the fetched AMI ID
@@ -27,6 +42,8 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]  # Attach the security group to control traffic
 
+  subnet_id = module.blog_vpc.public_subnets[0]  # Place the instance in the first public subnet
+  
   tags = {
     Name = "HelloWorld"  # Assign a name tag for easy identification
   }
@@ -37,7 +54,7 @@ module "blog_sg" {
   version = "5.3.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id  # Use the VPC ID from the VPC module
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
